fa1dfee35ad5e36403bc47af333f4721b9169d9b
Breania Harvey <harvey.breania@gmail.com>
2021-09-30 20:00:27 -0500

    html page generated

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
+// The rewuire statement is a built-in function that's globally available in node.js
+const fs = require('fs');
+// to use functions from other js files, use require('./relative-path)
+const generatePage = require('./src/page-template.js');
+
 // process is a global object that represents everything going on in the app; 
 // the argv property of process is an array that holds what was typed into the command line on execution so that the data can be captured and used w/in the app
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// assignment destructuring assigns elements of an array to variable names in a single expression
+const [name, github] = profileDataArgs;
 
-const printProfileData = (profileDataArr) => {
-    // this
-    for (let i=0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if(err) throw err;
 
-    console.log('==============');
-
-    // is the same as this
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output');
+})
---
+++ src/page-template.js
@@ -0,0 +1,25 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// assignment destructuring assigns elements of an array to variable names in a single expression
+const [name, github] = profileDataArgs;
+
+const generatePage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github/${github}">Github</a></h2>
+    </body>
+    </html>    
+    `;
+};
+
+// allows the functions to be used in other javascript files
+module.exports = generatePage;
