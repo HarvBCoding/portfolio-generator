ed959522511e790c2a76365da6ff682cce6f592c
Breania Harvey <harvey.breania@gmail.com>
2021-10-01 00:12:24 -0500

    multiple questions added

--- app.js
+++ app.js
@@ -1,17 +1,90 @@
 const inquirer = require('inquirer');
 
-inquirer
+const promptUser = () => {
     // the prompt method can receive an array of objects in it's argument known as the question object
     // the properties of the question object identify the type, name and question message
-    .prompt([
+    // the "about me" section
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username.'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
         }
-    ])
-    // the answers object is returned as a promise
-    .then(answers => console.log(answers));
+    ]);
+};
+
+const promptProject = portfolioData => {
+    console.log(`
+    Add a New Project
+    `);
+
+    // if there's no 'projects' array property, create one; [so that the array doesn't reset each time the function is called]
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'decription',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ]).then(projectData => {
+        // take the responses from the questions in promptproject and push the project data to the portfolioData.projects array
+        portfolioData.projects.push(projectData);
+        // if user wants to add more projects
+        if (projectData.confirmAddProject) {
+            // then ask questions about another project by passing the portfolioData array to promptProject
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
 // // The require statement is a built-in function that's globally available in node.js
 // const fs = require('fs');
 
