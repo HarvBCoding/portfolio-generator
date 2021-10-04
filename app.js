15f350a3089092f03f8adf4e0c1861a8763814ba
Breania Harvey <harvey.breania@gmail.com>
2021-10-04 13:40:25 -0500

    html fully generates from js

--- app.js
+++ app.js
@@ -1,4 +1,56 @@
 const inquirer = require('inquirer');
+// The require statement is a built-in function that's globally available in node.js
+const fs = require('fs');
+
+// to use functions from other js files, use require('./relative-path)
+// assigns the anonymous HTML template function in page-template.js to a variable
+const generatePage = require('./src/page-template.js');
+
+const mockData = {
+    name: 'Lernantino',
+    github: 'lernantino',
+    confirmAbout: true,
+    about:
+      'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+    projects: [
+      {
+        name: 'Run Buddy',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['HTML', 'CSS'],
+        link: 'https://github.com/lernantino/run-buddy',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'Taskinator',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['JavaScript', 'HTML', 'CSS'],
+        link: 'https://github.com/lernantino/taskinator',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'Taskmaster Pro',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+        link: 'https://github.com/lernantino/taskmaster-pro',
+        feature: false,
+        confirmAddProject: true
+      },
+      {
+        name: 'Robot Gladiators',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+        languages: ['JavaScript'],
+        link: 'https://github.com/lernantino/robot-gladiators',
+        feature: false,
+        confirmAddProject: false
+      }
+    ]
+  };
 
 const promptUser = () => {
     // the prompt method can receive an array of objects in it's argument known as the question object
@@ -129,26 +181,32 @@ const promptProject = portfolioData => {
             // then ask questions about another project by passing the portfolioData array to promptProject
             return promptProject(portfolioData);
         } else {
+            // otherwise, return the current user input
             return portfolioData;
         }
     });
 };
 
-promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
-    });
-// // The require statement is a built-in function that's globally available in node.js
-// const fs = require('fs');
+const pageHTML = generatePage(mockData);
 
-// // to use functions from other js files, use require('./relative-path)
-// const generatePage = require('./src/page-template.js');
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
 
-// const pageHTML = generatePage(name, github);
+    console.log('Page created! Check out the index.html in this directory to see it!')
+});
 
-// fs.writeFile('index.html', pageHTML, err => {
-//     if(err) throw err;
+// a promise chain is a series of functions that return Promises, allowing us to attach .then() methods to one another
+// prompt the user by calling the function
+// promptUser()
+    // after all quesions are answered in promptUser, prompt the user for information about projects they would like featured
+    // .then(promptProject)
+    // then take all the user answers and pass them in an array to the page-template js to create the html page
+    // .then(portfolioData => {
+        // const pageHTML = generatePage(portfolioData);
 
-//     console.log('Portfolio complete! Check out index.html to see the output');
-// });
+        // fs.writeFile('./index.html', pageHTML, err => {
+        //     if (err) throw new Error(err);
+
+        //     console.log('Page created! Check out the index.html in this directory to see it!');
+        // });
+    // });
